9a85f02e7747478c229e9e444fc855b9542306c0
Christian_Toloza <christiantoloza@wposs.com>
2022-01-28 15:49:40 -0500

    Favorites and PlayList views Finished

--- app/src/main/java/com/music_player/Splash.kt
+++ app/src/main/java/com/music_player/Splash.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
+import com.music_player.activities.PlayerActivity
 
 lateinit var animation: LottieAnimationView
 
--- app/src/main/java/com/music_player/activities/PlayerActivity.kt
+++ app/src/main/java/com/music_player/activities/PlayerActivity.kt
@@ -1,4 +1,4 @@
-package com.music_player
+package com.music_player.activities
 
 import android.os.Bundle
 import android.view.View
@@ -6,42 +6,41 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.music_player.R
 import com.music_player.adapters.OptionsAdapter
-import com.music_player.databinding.ActivityMainBinding
+import com.music_player.databinding.ActivityPlayerBinding
 import com.music_player.interfaces.OnClick
 import com.music_player.models.MenuModel
 import com.music_player.utils.logs.Logger
 
-
-class MainActivity : AppCompatActivity() {
-    private var totalSongs = 0
-    private var binding: ActivityMainBinding? = null
+class PlayerActivity : AppCompatActivity() {
+    private var binding: ActivityPlayerBinding? = null
+    private var sName = "Unknown Song"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_player)
+        binding!!.songName = sName
         initOptions()
-        binding!!.total = totalSongs
     }
 
     private fun initOptions() {
         val list = arrayListOf<MenuModel>()
-        list.add(MenuModel(getString(R.string.shuffle), R.drawable.ico_shuffle))
-        list.add(MenuModel(getString(R.string.favourites), R.drawable.ico_favorite))
-        list.add(MenuModel(getString(R.string.playlist), R.drawable.ico_playlist))
+        list.add(MenuModel(getString(R.string.repeat), R.drawable.ico_repeat))
+        list.add(MenuModel(getString(R.string.equalizer), R.drawable.ico_equalizer))
+        list.add(MenuModel(getString(R.string.timer), R.drawable.ico_timer))
+        list.add(MenuModel(getString(R.string.share), R.drawable.ico_share))
 
         val recycler = findViewById<View>(R.id.opts) as RecyclerView
-        recycler.adapter = OptionsAdapter(list, false, onClick)
+        recycler.adapter = OptionsAdapter(list, true, onClick)
         recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         recycler.setHasFixedSize(true)
         recycler.isNestedScrollingEnabled = false
-        totalSongs = 400
     }
 
     private val onClick = object : OnClick {
         override fun onCLick(v: View) {
             Logger.error("Data: ${v.contentDescription}")
         }
-
     }
 }
